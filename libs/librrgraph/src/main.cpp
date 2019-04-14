e91d2e94b885aa76563fac40b75c533e8cfad4f0
tangxifan <tangxifan@gmail.com>
2019-04-14 22:58:53 +0800

    add main file for librrgraph

--- libs/librrgraph/src/main.cpp
+++ libs/librrgraph/src/main.cpp
@@ -1,17 +1,77 @@
 /*
- Test libarchfpga, try reading an architecture and print the results to a file
+ Test librrgraph, try reading an architecture and generate rr_graph 
 
- Date: February 19, 2009
- Author: Jason Luu
+ Date: April 15, 2019
+ Author: Xifan Tang
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "vtr_error.h"
+#include "vtr_memory.h"
+#include "read_xml_arch_file.h"
+#include "echo_arch.h"
+
+void print_help() {
+  printf("\n---------------------------------------------------------------------------------------\n");
+  printf("read_arch - Read a VPR architecture file and output internal data structures\n");
+  printf("\n");
+  printf("Usage: read_arch <arch_file.xml> <timing_driven (0|1)> <output_file>\n");
+  printf("\n");
+  printf("  ex: read_arch k4_n10.xml 1 arch_data.out\n");
+  printf("      Read timing-driven architecture k4_n10.xml and output the results to arch_data.out\n");
+  printf("\n---------------------------------------------------------------------------------------\n");
+}
+
 int main(int argc, char **argv) {
 
-    printf("Done\n");
+  try {
+    /* Read options */
+    if (2 != argc - 1) { /* First argv is the name of program */
+      printf("Error: Unexpected # of arguments. Expected 2 found %d arguements\n",
+             argc);
+      print_help();
+      return 1;
+    }
 
-	return 0;
+    printf("------------------------------------------------------------------------------\n");
+    printf("- Read architecture file and print library data structures into an output file\n");
+    printf("------------------------------------------------------------------------------\n\n");
+
+    printf("Inputs: \n"
+           "architecture %s \n"
+           "timing_driven %d \n"
+           "output file %s\n", argv[1], atoi(argv[2]), argv[3]);
+    printf("Reading in architecture\n");
+
+    /* Read architecture XML and build_rr_graph options */
+    t_arch* arch = (t_arch*) vtr:calloc(1, sizeof(t_arch));
+    t_type_descriptor* types = NULL;
+    int numTypes;
+
+    XmlReadArch(argv[1], atoi(argv[2]), arch, &types, &numTypes);
+
+    printf("Printing Results\n");
+
+    EchoArch(argv[3], types, numTypes, arch);
+
+    /* Build rr_graph  */
+    t_rr_graph rr_graph;
+    rr_graph.build_rr_graph(arch, grid);
+
+    /* Optional: dump the built rr_graph  */
+    rr_graph.dump_rr_graph();
+
+    /* Deconstruct and free */
+    free(arch);
+  } catch (vtr::VtrError& vtr_error) {
+    printf("Failed to build rr_graph from architecture %s: %s\n", argv[1], vtr_error.what());
+    return 1;
+  }
+
+  printf("Done\n");
+
+  return 0;
 }
 
