8339989fbf4f623809a07734e4158353bc8e9006
tangxifan <tangxifan@gmail.com>
2019-04-14 17:22:05 +0800

    Update CMake scripts and ready for lib build-up

---
+++ libs/librrgraph/src/main.cpp
@@ -0,0 +1,17 @@
+/*
+ Test libarchfpga, try reading an architecture and print the results to a file
+
+ Date: February 19, 2009
+ Author: Jason Luu
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+
+    printf("Done\n");
+
+	return 0;
+}
+
